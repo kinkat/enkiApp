06712aad3368bd44390eae35d97550a830f32a30
kinkat <kingatomaszewska@gmail.com>
2016-04-11 13:23:38 +0200

    Add template and yoman files

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('enkiApp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
